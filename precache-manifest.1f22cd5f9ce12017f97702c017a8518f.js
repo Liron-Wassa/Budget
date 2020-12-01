8275133058ccf9bf4dcc2efbdc441f083b6485b7
Liron Wassa <khruiuuxv@gmail.com>
2020-12-01 14:13:03 +0200

    Updates

---
+++ precache-manifest.1f22cd5f9ce12017f97702c017a8518f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71350a935aaa6259115186b6b3eaeac",
+    "url": "/Budget/index.html"
+  },
+  {
+    "revision": "5fd9c73fab2900192fce",
+    "url": "/Budget/static/css/main.bcf08685.chunk.css"
+  },
+  {
+    "revision": "72117f820643bf300610",
+    "url": "/Budget/static/js/2.9ccf5250.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Budget/static/js/2.9ccf5250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd9c73fab2900192fce",
+    "url": "/Budget/static/js/main.dd574e04.chunk.js"
+  },
+  {
+    "revision": "c3095cd33523b5ea8ffc",
+    "url": "/Budget/static/js/runtime-main.abe3df5c.js"
+  }
+]);
